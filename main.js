df1e404bbda284f1fa9c57c511e60ba9e897f894
Kate <katesava92@gmail.com>
2020-06-04 13:47:41 +0300

    change values

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-let money = 1;
+let money = 10;
 let income = 2;
-let addExpences = 3;
-let deposit = 4;
-let mission = 5;
+let addExpences = 300;
+let deposit = 1;
+let mission = 9;
 let period = 6;
 
-alert('Hello, world');
-console.log('Hello' + money);
+alert('Hello, world!');
+console.log('Hello ' + money);
