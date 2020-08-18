a3d3d408f494125e49d61b7e7a3a7c37278564ca
Kate <katesava92@gmail.com>
2020-08-18 23:08:57 +0300

    :fix: fix mistakes

--- Homework2/easy/main.js
+++ Homework2/easy/main.js
@@ -1,18 +1,16 @@
 "use strict";
-let money = 1000,
+const money = 1000,
     income = '500',
     addExpences = 'Интернет, Еда, Коммуналка, Мобильная Связь',
     deposit = false,
     mission = 50000,
-    period = 12;
+    period = 12,
+    budgetDay = money / 30;
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+console.log(typeof money, income, deposit);
 console.log(addExpences.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей');
 console.log(addExpences.toLowerCase());
 console.log(addExpences.split(', '));
-let budgetDay = money / 30;
 console.log(budgetDay);
